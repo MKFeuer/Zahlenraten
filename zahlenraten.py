2aa7fe2a72d526a205c27078f329db013cd7e729
MK Feuer <91680831+MKFeuer@users.noreply.github.com>
2023-12-07 08:35:13 +0100

    Update zahlenraten.py

--- zahlenraten.py
+++ zahlenraten.py
@@ -3,22 +3,21 @@ import random
 zufallszahl = random.randint(1, 100)
 
 def zahlenraten():
+    zufallszahl = random.randint(1, 100)
     zuege = 3
     while zuege != 0:
         user_eingabe = int(input("Bitte geben sie eine Zahl ein \n"))
-        if user_eingabe > zufallszahl:
-            print("Die gesuchte Zahl ist kleiner")
-            zuege -= 1
-            print("Sie haben noch " ,zuege, "Züge")
-
-        elif user_eingabe < zufallszahl:
-            print("Die gesuchte Zahl ist größer")
-            zuege -= 1
-            print("Sie haben noch " ,zuege, "Züge")
-
-        elif user_eingabe == zufallszahl:
+        if user_eingabe == zufallszahl:
             print ("Glückwunsch: Sie haben gewonnen")
-            return
+            break
+
+        elif user_eingabe > zufallszahl:
+            print("Die gesuchte Zahl ist kleiner")
+        else:
+            print("Die gesuchte Zahl ist größer")
+        
+        zuege -= 1
+        print("Sie haben noch " ,zuege, "Züge")   
 
     print("Sorry, Sie haben leider Verloren. Die gesuchte Zahl wäre ", zufallszahl, "gewesen")
 
